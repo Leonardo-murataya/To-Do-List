79f54ba76dabfee01009d78cf488f8d2ded8d5de
Bryan Leonardo Murataya Moreno <bmurataya@ucol.mx>
2024-10-03 13:55:10 -0600

    Primeros estilos, forma de la pagina y mas php

--- includes/database.php
+++ includes/database.php
@@ -1,5 +1,7 @@
 <?php
+// Conectar a la base de datos
 $db = mysqli_connect('localhost', 'root', 'root', 'ToDoList');
+// Verificar la conexión
 if (!$db) {
     die("No se puede conectar a MySQL: " . mysqli_connect_error());
 }
--- index.php
+++ index.php
@@ -1,11 +1,45 @@
-<!DOCTYPE html>
-<html lang="es-mx">
+<!doctype html>
+<html lang="es">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>ToDo List</title>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="src/CSS/Normalize.css">
+    <!-- fuente de letra (pendiente) -->
+    <link rel="stylesheet" href="src/CSS/style.css.css">
+    <title>To Do List</title>
 </head>
 <body>
-  
+<header class="header">
+    <div class="contenedor__header">
+        <div class="barra">
+            <a href="index.php" class="logo">To Do List</a>
+
+            <nav class="navegacion">
+                <a href="#" class="navegacion__enlace">Registro</a>
+                <a href="#" class="navegacion__enlace">Inicio de Secion</a>
+            </nav>
+        </div>
+    </div>
+</header>
+
+<main class="contenedor main">
+    <div class="barra__lsitas">
+        <h2>Listas</h2>
+
+        <div class="contenedor__btn">
+            <button class="btn btn__crear">Crear Lista</button>
+            <button class="btn btn__editar">Editar Lista</button>
+            <button class="btn btn__eliminar">Eliminar Lista</button>
+        </div>
+    </div>
+
+    <section class="lsitas contenedor">
+
+    </section>
+
+</main>
+
 </body>
 </html>
